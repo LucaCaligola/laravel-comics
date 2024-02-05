c9fb4823633385d720e929124ddff49de6cf8ee2
LucaCaligola <luca.caligola@gmail.com>
2024-02-05 16:12:31 +0100

    working on the layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DC Comics</title>
+
+    @vite('resources/js/app.js')
+
+  
+</head>
+<body>
+   @include('partials.header')
+
+   <main>
+
+
+    <div class="background">
+        <img src="{{ Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
+    </div>
+
+    <div class="background-dark">
+
+    </div>
+
+   </main>
+    
+
+   @include('partials.footer')
+</body>
+</html>
